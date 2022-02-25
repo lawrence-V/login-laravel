6d5019bf3b317f22879c70002bacbae05c3e6f32
lawrence-V <lawrencevaldez027@gmail.com>
2022-02-25 18:49:25 +0800

    push

---
+++ login-form/app/Http/Controllers/MainController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class MainController extends Controller
+{
+    function login() {
+        return view('auth.login');
+    }
+
+    function register() {
+        return view('auth.register');
+    }
+    function save(Request $request) {
+        
+        // validate request
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:admins',
+            'password' => 'required|min:5|max:12',
+        ]);
+
+        // Insert data into database
+        $admin = new Admin;
+        $admin->name = $request->name;
+        $admin->email = $request->email;
+        $admin->password = Hash::make($request->password);
+        $save = $admin->save();
+
+        if($save){
+            return back()->with('success','New user have been successfully created');
+        }else {
+            return back()->with('error','Something went wrong try again');
+        }
+    }
+
+    function check(Request $request) {
+        // validate request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:5|max:12',
+        ]);
+
+        $userInfo = Admin::where('email','=', $request->email)->first();
+
+        if(!$userInfo){
+            return back()->with('error', ' we do not recognize your email address');
+        }
+        else {
+            // ceheck password
+            if(Hash::check($request->password, $userInfo->password)) {
+                $request->session()->put('LoggedUser', $userInfo->id);
+                return redirect('admin/dashboard');
+            }else {
+                return back()->with('error', 'Incorrect password');
+            }
+        }
+    }
+
+    function logout() {
+        if(session()->has('LoggedUser')){
+            session()->pull('LoggedUser');
+            return redirect('/auth/login');
+        }
+    }
+
+    function dashboard() {
+        $data = ['LoggedUserInfo'=>Admin::where('id', '=', session('LoggedUser'))->first()];
+        return view('admin.dashboard', $data);
+    }
+
+    function settings() {
+        $data = ['LoggedUserInfo'=>Admin::where('id', '=', session('LoggedUser'))->first()];
+        return view('admin.settings', $data);
+    }
+
+    function profile() {
+        $data = ['LoggedUserInfo'=>Admin::where('id', '=', session('LoggedUser'))->first()];
+        return view('admin.profile', $data);
+    }
+
+    function staff() {
+        $data = ['LoggedUserInfo'=>Admin::where('id', '=', session('LoggedUser'))->first()];
+        return view('admin.staff', $data);
+    }
+
+    
+}
+
+
+
---
+++ login-form/resources/views/auth/register.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row" style="margin-top: 45px">
+            <div class="col-md-4 col-md-offset-4">
+                <h4>Login | Custom Auth</h4>
+                <hr>
+                <form action="{{ route('auth.save') }}" method="post">
+                    @if(Session::get('success'))
+                        <div class="alert alert-success">
+                            {{Session::get('success')}}
+                        </div>
+                    @endif
+
+                    @if(Session::get('error'))
+                        <div class="alert alert-danger">
+                            {{Session::get('error')}}
+                        </div>
+                    @endif
+
+                    @csrf
+                    <div class="form-group">
+                        <label for="">Name</label>
+                        <input type="text" class="form-control" name="name" placeholder="Enter your Name" value="{{ old('name')}}">
+                        <span class="text-danger">@error('name'){{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="text" class="form-control" name="email" placeholder="Enter your email address" value="{{ old('email')}}">
+                        <span class="text-danger">@error('email'){{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Enter your password" >
+                        <span class="text-danger">@error('password'){{ $message }} @enderror</span>
+                    </div>
+                    <button type="submit" class="btn btn-block btn-primary " style="margin-top: 45px" >Sign Up</button>
+                    <br>
+                    <a href="{{ route('auth.login')}}">I ALready have an account , sign in</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
